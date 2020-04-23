e97ef8799777e9c36df96ac06966cec5234d2309
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-04-23 18:52:15 -0300

    authController added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
 
 const authconfig = require("../config/auth");
+const admins = require('../config/admins');
 
 const router = express.Router();
 
@@ -12,15 +14,25 @@ function generatetoken(params = {}){
 }
 
 router.post('/login', async (req, res) => {
-	const{ accessToken } = req.headers;
+	const { accesstoken } = req.headers;
 
-	if (!accessToken)
+	if (!accesstoken)
 		return res.status(400).send({ error : "Access token not provided"});
 
-  
+  const path = `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accesstoken}`;
+  const response = undefined;
+  try {
+    response = await axios.get(path);
+  } catch (err) {
+    return res.status(403).json({ error: 'Access Forbidden' })
+  }
+
+  const user = response.data;
+  if (!(admins.includes(user.email)))
+    return res.status(401).json({ error: 'Você não é administrador.' })
 
 	return res.send({ user,
-	 token: generatetoken({id: user.id}),});
+	  token: generatetoken({id: user.id}),});
 });
 
 module.exports = app => app.use('/auth', router);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 require('./controllers/authController')(app);
 
 
-app.listen(3000);
+app.listen(3333);
